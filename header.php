a0589bdff85b6407f06cbcad3c09cf61296fd55d
Adeline Levionnois <64022509+ad-levionnois@users.noreply.github.com>
2020-05-27 17:00:39 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -10,41 +10,52 @@
     <title></title>
   </head>
   <body>
-    <header>
+
+<header>
       <div class="contenair-fluid">
         <div class="col-sm">
-<nav class="navbar navbar-expand-lg navbar-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Menu</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Blog</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Shop</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-</nav>
-</div>
-</div>
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Shop</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                  </ul>
+              </nav>
+            </div>
+          </div>
+
+          <img class="logo-header"src="img/logo_coffee.png" alt="logo">
+            <div class="contenair">
+             <h1>Coffee</h1>
+               <p class="headline"> Everything You Love About Coffee </p>
+                 <p class="description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam.
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <a href="#" class="btn btn-primary">Order Now</a>
+             </div>
 </header>
 
 
@@ -55,65 +66,68 @@
         		<div class="col-sm-2">
             		<img src="img/dark_coffees.png" class="card-img-top" alt="dark_coffees">
         		</div>
-       		<div class="col-sm-2">
-       			<h4 class="card-title">Dark Coffees</h4>
-              		<div class="card-body">
-                		<p> Lorem ipsum dolor sit amet, consectetur adipis
-                  		cing elit, sed do eiusmod tempor incididunt ut
-                  		labore et dolore magna aliqua. Ut enim ad
-                 		minim veniam, quis nostrud exercitation
-                 		ullamco laboris nisi ut aliquip ea commodo
-                  		consequat. Duis aut dolor in reprehenderit
-                  		in voluptate velit esse cillum dolore eu fugiat
-                  		nulla pariatur.</p>
-                			<a href="#" class="btn btn-primary">Order Now</a>
-        			</div>
-      		</div>
-  		<div class="col-sm-2">
-        	<img src="img/cold_coffees.png" class="card-img-top" alt="cold_coffees">
-  		</div>
-  	<div class="col-sm-2">
-		<h4 class="card-title">Cold Coffees</h4>
-         	<div class="card-body">
-            	<p>Lorem ipsum dolor sit amet, consectetur adipis
-              	cing elit, sed do eiusmod tempor incididunt ut
-              	labore et dolore magna aliqua. Ut enim ad
-              	minim veniam, quis nostrud exercitation
-              	ullamco laboris nisi ut aliquip ea commodo
-              	consequat. Duis aut dolor in reprehenderit
-              	in voluptate velit esse cillum dolore eu fugiat
-              	nulla pariatur.</p>
-            		<a href="#" class="btn btn-primary">Order Now</a>
-          	</div>
-    </div>
-<div class="col-sm-2">
-    <img src="img/hot_coffees.png" class="card-img-top" alt="hot_coffees">
-</div>
-<div class="col-sm-2">
-    <h4 class="card-title">Hot Coffees</h4>
-    	<div class="card-body">
-        	<p>Lorem ipsum dolor sit amet, consectetur adipis
-          	cing elit, sed do eiusmod tempor incididunt ut
-          	labore et dolore magna aliqua. Ut enim ad
-          	minim veniam, quis nostrud exercitation
-          	ullamco laboris nisi ut aliquip ea commodo
-          	consequat. Duis aut dolor in reprehenderit
-          	in voluptate velit esse cillum dolore eu fugiat
-          	nulla pariatur.</p>
-        		<a href="#" class="btn btn-primary">Order Now</a>
-    	</div>
-</div>
-</div>
-</div>
+       		   <div class="col-sm-2">
+       			     <h4 class="card-title">Dark Coffees</h4>
+              		 <div class="card-body">
+                		   <p> Lorem ipsum dolor sit amet, consectetur adipis
+                  		     cing elit, sed do eiusmod tempor incididunt ut
+                  		     labore et dolore magna aliqua. Ut enim ad
+                 		       minim veniam, quis nostrud exercitation
+                 		       ullamco laboris nisi ut aliquip ea commodo
+                  		     consequat. Duis aut dolor in reprehenderit
+                  		     in voluptate velit esse cillum dolore eu fugiat
+                  		     nulla pariatur.</p>
+                			        <a href="#" class="btn btn-primary">Order Now</a>
+        			     </div>
+      		   </div>
+  		   <div class="col-sm-2">
+        	  <img src="img/cold_coffees.png" class="card-img-top" alt="cold_coffees">
+  		   </div>
+  	      <div class="col-sm-2">
+		          <h4 class="card-title">Cold Coffees</h4>
+         	       <div class="card-body">
+            	      <p>Lorem ipsum dolor sit amet, consectetur adipis
+              	       cing elit, sed do eiusmod tempor incididunt ut
+              	       labore et dolore magna aliqua. Ut enim ad
+              	       minim veniam, quis nostrud exercitation
+              	       ullamco laboris nisi ut aliquip ea commodo
+              	       consequat. Duis aut dolor in reprehenderit
+              	       in voluptate velit esse cillum dolore eu fugiat
+              	       nulla pariatur.</p>
+            		            <a href="#" class="btn btn-primary">Order Now</a>
+          	     </div>
+          </div>
+          <div class="col-sm-2">
+            <img src="img/hot_coffees.png" class="card-img-top" alt="hot_coffees">
+          </div>
+          <div class="col-sm-2">
+            <h4 class="card-title">Hot Coffees</h4>
+    	         <div class="card-body">
+        	        <p>Lorem ipsum dolor sit amet, consectetur adipis
+          	         cing elit, sed do eiusmod tempor incididunt ut
+          	         labore et dolore magna aliqua. Ut enim ad
+          	         minim veniam, quis nostrud exercitation
+          	         ullamco laboris nisi ut aliquip ea commodo
+          	         consequat. Duis aut dolor in reprehenderit
+          	         in voluptate velit esse cillum dolore eu fugiat
+          	         nulla pariatur.</p>
+        		              <a href="#" class="btn btn-primary">Order Now</a>
+    	          </div>
+              </div>
+            </div>
+          </div>
 </section>
+
+<section class="offer">
   <div class="row">
     <div class="col-md-6">
-      <img class="offer" src="img/offer.png" alt="offer">
+      <img class="free" src="img/offer.png" alt="offer">
     </div>
-</div>
-<div class="container">
-  <div class="about_us">
-  <h2> About us </h2>
+</section>
+
+<section class="about_us">
+  <div class="container">
+    <h2> About us </h2>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -127,62 +141,66 @@
         esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
         deserunt mollit anim id est laborum</p>
   </div>
-</div>
+</section>
+
 <section class="our_branches">
     <div class="container-fluid">
       <h2>Our branches</h2>
-      <div class="row">
-        <div class="col-sm">
-          <div class="card">
-            <img src="img/new_york.png" class="card-img-top" alt="new_york">
-            <div class="card-body">
-              <h4 class="card-title">New York</h4>
+        <div class="row">
+          <div class="col-sm">
+            <div class="card">
+              <img src="img/new_york.png" class="card-img-top" alt="new_york">
+                <div class="card-body">
+                  <h4 class="card-title">New York</h4>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm">
+              <div class="card">
+                <img src="img/texas.png" class="card-img-top" alt="texas">
+                  <div class="card-body">
+                    <h4 class="card-title">Texas</h4>
+                  </div>
+              </div>
+            </div>
+            <div class="col-sm">
+              <div class="card">
+                <img src="img/new_jorsy.png" class="card-img-top" alt="new_jorsy">
+                  <div class="card-body">
+                    <h4 class="card-title">New Jorsy</h4>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-    <div class="col-sm">
-      <div class="card">
-        <img src="img/texas.png" class="card-img-top" alt="texas">
-        <div class="card-body">
-          <h4 class="card-title">Texas</h4>
-        </div>
-      </div>
-    </div>
-  <div class="col-sm">
-    <div class="card">
-      <img src="img/new_jorsy.png" class="card-img-top" alt="new_jorsy">
-      <div class="card-body">
-        <h4 class="card-title">New Jorsy</h4>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
 </section>
-<div class="hours_open">
+
+<section class="hours_open">
   	<h2> We are open </h2> </br>
-<div class="row">
-  	<div class="col-sm">
-     	<div class="container">
-        	<p> Monday-Friday </br>
-              8:00 am - 11:30 pm</p>
-       	</div>
-    </div>
-    <div class="col-sm">
-    	<div class="container">
-      		<p>Sunday and Saturday </br>
-        	9:00 am - 10:30 pm </p>
-    	</div>
-  </div>
-  </div>
-</div>
-<div class="container">
+      <div class="row">
+  	     <div class="col-sm">
+     	     <div class="container">
+        	    <p> Monday-Friday </br>
+                8:00 am - 11:30 pm</p>
+       	   </div>
+         </div>
+         <div class="col-sm">
+    	      <div class="container">
+      		      <p>Sunday and Saturday </br>
+        	         9:00 am - 10:30 pm </p>
+    	      </div>
+          </div>
+        </div>
+</section>
+
+<section class="customer_reviews">
+  <div class="container">
   	<div class="customer_reviews">
     	<h2>Customer Reviews </h2>
-      		<div class="row">
-        		<div class="col-md-6">
-          			<img src="img/customer_reviews.png" class="card-img-top" alt="customer_reviews">
-        		</div>
+      	<div class="row">
+        	<div class="col-md-6">
+          	<img src="img/customer_reviews.png" class="card-img-top" alt="customer_reviews">
+        	</div>
       			<div class="col-md-6">
         			<h3>John Peter </h3>
           				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
@@ -199,4 +217,5 @@
       			</div>
     		</div>
     </div>
-</div>
+  </div>
+</section>
